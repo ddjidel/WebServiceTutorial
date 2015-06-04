c5445e1df26da0bd809ef73f4b895f317b489853
Dalil Djidel <dalil.djidel@hotmail.com>
2015-06-04 16:15:39 +0200

    init

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ContactManager.Models;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+        public HttpResponseMessage Post(string id, string name)
+        {
+            Contact contact = new Contact();
+            contact.Id =  Convert.ToInt32(id);
+            contact.Name = name;
+
+            contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
---
+++ ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ContactManager.Models;
+using System.Web;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+        private const string CacheKey = "ContactStore";
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact
+                        {
+                            Id = 1,
+                            Name = "Manuel Altermatt"
+                        },
+                        new Contact
+                        {
+                            Id = 2,
+                            Name = "Silvia Gonner"
+                        },
+                        new Contact
+                        {
+                            Id = 3,
+                            Name = "Michael Griebsch"
+                        },
+                        new Contact
+                        {
+                            Id = 4,
+                            Name = "Dalil Djidel"
+                        }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
+            return new Contact[]
+            {
+                new Contact
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
+            };
+        }
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
